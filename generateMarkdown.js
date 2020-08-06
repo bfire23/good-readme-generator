c9f8bf773bf861b1d8388aba068ceb4d7fc49ee9
Blake Arambula <bfire23@gmail.com>
2020-08-05 17:22:04 -0700

    updated homework

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -2,6 +2,38 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  # Table of Contents
+
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributions](#contributions)
+  * [Tests](#tests)
+  * [License](#license)
+  * [Questions](#questions)
+  
+   
+  ## Installation:
+    ${data.installation}
+  ## Usage:
+    ${data.usage}
+  ## Contributions:
+    ${data.contributions}
+  ## Tests:
+    ${data.tests}
+    
+  ## Questions:
+    Please visit my GitHub page for any questions about the project. 
+    Here is the link:
+
+    [GitHub profile](https://github.com/${data.username})
+
+    If you have anymore questions, please email me at ${data.email}
+
+
+
+
+
 `;
 }
 
--- index.js
+++ index.js
@@ -1,7 +1,79 @@
 const inquirer = require("inquirer");
+const util = require("util");
+const fs = require("fs");
 
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        message: "what is the title of your project?",
+        name: "Title"
+      },
+      {
+        type: "input",
+        message: "Please decrible your project?",
+        name: "Description",
+        
+      },
+      {
+        type: "input",
+        message: "What are the steps required to install your project?",
+        name: "Installation",
+        
+      },
+      {
+        type: "input",
+        message: "choose a license",
+        Choices: [
+            "GNU GPLv3",
+             "MIT",
+              "Apache"
+        ],
+        name: "License",
+        
+      },
+      // {
+      //   type: "input",
+      //   message: "Please use table of contents.",
+      //   Choices: [
+      //     "Description",
+      //     "Installation",
+      //     "Usage",
+      //     "Contributions",
+      //     "tests"
+      //   ]
+        
+      // },
+      {
+        type: "input",
+        message: "Provide instructions and examples for use.",
+        name: "Usage",
+        
+      },
+      {
+        type: "input",
+        message: "Who has contributed to this project?",
+        name: "Contributions",
+        
+      },
+      {
+        type: "input",
+        message: "How do you test this project?",
+        name: "Tests",
+        
+      },
+      {
+        type: "input",
+        message: "Please enter your GitHub username.",
+        name: "Username",
+        
+      },
+      {
+        type: "input",
+        message: "Please enter your email address.",
+        name: "email",
+        
+      }
 
 
 ];
@@ -13,7 +85,6 @@ function writeToFile(fileName, data) {
 // function to initialize program
 function init() {
 
-}
-
-// function call to initialize program
-init();
+}   
+    // function call to initialize program
+    init();
